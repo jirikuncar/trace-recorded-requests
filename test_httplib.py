995019e3d982810c3e700324cae3747d5b749c93
Jiri Kuncar <jiri.kuncar@gmail.com>
2020-12-16 11:00:53 +0100

    reproduced

--- test_httplib.py
+++ test_httplib.py
@@ -7,9 +7,9 @@ import http.client
 import urllib3
 import pytest
 
+@pytest.mark.parametrize("id_", range(100))
 @pytest.mark.parametrize("url", ("www.datadoghq.com", "docs.datadoghq.com"))
-def test_httplib(vcr_cassette, ddspan, url):
-    for _ in range(100):
+def test_httplib(vcr_cassette, ddspan, id_, url):
         conn = http.client.HTTPSConnection(url)
         conn.request("GET", "/")
         with tracer.trace("request"):
@@ -18,10 +18,10 @@ def test_httplib(vcr_cassette, ddspan, url):
         assert 'OK' == r1.reason
 
 
+@pytest.mark.parametrize("id_", range(100))
 @pytest.mark.parametrize("url", ("www.datadoghq.com", "docs.datadoghq.com"))
-def test_urllib3(vcr_cassette, ddspan, url):
+def test_urllib3(vcr_cassette, ddspan, id_, url):
     http = urllib3.PoolManager()
-    for _ in range(100):
-        with tracer.trace("request"):
-            r = http.request("GET", url)
-        assert 200 == r.status
+    with tracer.trace("request"):
+        r = http.request("GET", url)
+    assert 200 == r.status
